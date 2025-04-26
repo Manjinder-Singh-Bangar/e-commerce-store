b8c8504c92f197fc0fe909a9d8ca849751f8653d
Manjinder Singh Bangar <119203129+Manjinder-Singh-Bangar@users.noreply.github.com>
2025-04-26 16:05:51 -0400

    coupon route, model and controller is done

---
+++ Backend/controllers/cart.controller.js
@@ -0,0 +1,83 @@
+import Product from "../models/product.model.js";
+
+export const addToCart = async (req, res) => {
+    try {
+        const {productId} = req.body;
+        const user = req.user;
+
+        const existingItem = user.cartItems.find(item => item.id === productId);
+
+        if(existingItem){
+            existingItem.quantity += 1;
+        }else{
+            user.cartItems.push(productId)
+        }
+
+        await user.save();
+        return res.json({data: user.cartItems})
+    } catch (error) {
+        console.error("Error occured while adding to cart", error)
+        return res.status(401).json({message: error.message || "Error occured while add to cart" })
+    }
+}
+
+export const removeAllFromCart = async (req, res) => {
+    try {
+        const {productId} = req.body;
+
+        const user = req.user;
+
+        if(!productId) {
+            user.cartItems = [];
+        }else{
+            user.cartItems = user.cartItems.filter((item) = item !== productId)
+        }
+        await user.save();
+        return res.json(user.cartItems);
+    } catch (error) {
+        console.error("Error while removing item from cart ",error.message)
+        return res.status(500).json({message: error.message || "Something went wrong while removing item from cart"})
+    }
+}
+
+export const updateQuantity = async (req, res) => {
+    try {
+        const {id:productId} = req.params;
+        const {quantity} = req.body
+
+        const user = req.user;
+        const existingItem = user.cartItems.find((item) => item.id === productId)
+
+        if(existingItem){
+            if(quantity === 0){
+                user.cartItems = user.cartItems.filter((item) => item !== productId)
+                await user.save();
+                return res.json({data: user.cartItems});
+            }
+
+            existingItem.quantity = quantity;
+            await user.save();
+            return res.json({data: user.cartItems})
+        }
+    } catch (error) {
+        console.error("Error while updating the quantity", error)
+        return res.status(500).json({message: error.message || "Something went wrong while updating the quantity"})
+    }
+}
+
+export const getCartProducts = async (req, res) => {
+    try {
+        const products = await Product.find({_id: {$in: req.user.cartItems}})
+
+        const cartItems = products.map((product) => {
+            const item = req.user.cartItems.find((cartItems) => cartItems.id === product.id);
+            return {...product.toJSON(), quantity: item.quantity};
+        })
+
+        return res.json(cartItems);
+    } catch (error) {
+        console.error("Error in while getting the items in the cart", error)
+        return res.status(500).json({message: "Something went wrong while getting the cart products"});
+        
+    }
+}
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,9 @@ import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cartRoutes from "./routes/cart.route.js"
+import couponRoutes from "./routes/coupon.route.js"
+
 const app = express();
 
 app.use(express.json())
@@ -12,6 +15,9 @@ configDotenv()
 
 app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes)
+app.use("/api/cart",cartRoutes )
+app.use("/api/coupon", couponRoutes)
+
 app.listen(process.env.PORT, ()=> {
     connectDb();
     console.log("Server is running on the port",process.env.PORT);
